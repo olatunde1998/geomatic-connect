da26e3691dfd23a6920e45ef5e8c3520e361ffe4
geodevcode <geodevinstitute@gmail.com>
2024-08-15 11:37:17 +0100

    dashboard layout created

---
+++ app/(StudentDashboard)/student-dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="pt-10">This is Students Dashboard Page</p>
+
+      <p>Welcome to Geomatic Connect</p>
+    </main>
+  );
+}
