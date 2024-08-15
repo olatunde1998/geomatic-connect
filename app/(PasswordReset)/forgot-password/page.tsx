bf49dd4594f365cfe44b41e52cef3f91fd51e4a5
geodevcode <geodevinstitute@gmail.com>
2024-08-15 11:42:28 +0100

    Password reset page added

---
+++ app/(PasswordReset)/forgot-password/page.tsx
@@ -0,0 +1,3 @@
+export default async function ForgotPasswordPage() {
+  return <div>This is forgot password page</div>;
+}
---
+++ app/(PasswordReset)/reset-password/page.jsx
@@ -0,0 +1,3 @@
+export default async function ResetPasswordPage() {
+  return <div>This is Reset password page</div>;
+}
