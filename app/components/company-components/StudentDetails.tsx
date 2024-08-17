4c3a120c07d53cf67bbd19963da7d4e05616f456
geodevcode <geodevinstitute@gmail.com>
2024-08-17 11:32:15 +0100

    StudentDetails created

---
+++ app/components/company-components/StudentDetails.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useState } from "react";
+import StudentProfile from "./StudentProfile";
+import StudentProject from "./StudentProject";
+import StudentInstitution from "./StudentInstitution";
+import DetailsTab from "./DetailsTab";
+
+interface StudentDetailsProps {
+  studentId?: any;
+}
+
+export default function StudentDetails({ studentId }: StudentDetailsProps) {
+  const [selectedTab, setSelectedTab] = useState("Profile");
+  return (
+    <>
+      <div className="mt-24 mb-10 items-center justify-between bg-[#ECF1F7] lg:flex p-4  lg:my-20 xl:my-10">
+        This is Student Id here === {studentId}
+        {/*=====Complete  Profile Tabs ====== */}
+        <div>
+          <DetailsTab
+            selectedTab={selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
+        </div>
+      </div>
+      {/* ================Body section ===========  */}
+      <section className="mt-8">
+        {/* ==== Profile ====*/}
+        <div>
+          {selectedTab === "Profile" && (
+            <section>
+              <StudentProfile />
+            </section>
+          )}
+        </div>
+
+        {/* ==== Project ====*/}
+        <div>
+          {selectedTab === "Project" && (
+            <section>
+              <StudentProject />
+            </section>
+          )}
+        </div>
+
+        {/* ==== Institution ====*/}
+        <div>
+          {selectedTab === "Institution" && (
+            <section>
+              <StudentInstitution />
+            </section>
+          )}
+        </div>
+      </section>
+    </>
+  );
+}
