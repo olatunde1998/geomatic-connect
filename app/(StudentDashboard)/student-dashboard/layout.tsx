cb42195b2729f83ee4b7c78e4a49c21311afae24
geodevcode <geodevinstitute@gmail.com>
2024-08-17 08:28:07 +0100

    Navbar imported

--- app/(StudentDashboard)/student-dashboard/layout.tsx
+++ app/(StudentDashboard)/student-dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/app/components/navbar/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-[#F8F9FC]">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
