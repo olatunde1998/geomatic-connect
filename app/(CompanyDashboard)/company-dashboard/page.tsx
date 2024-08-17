b0f6be5779e63cbbe6cfd04603b9fe2cbc0ecb38
geodevcode <geodevinstitute@gmail.com>
2024-08-17 11:23:38 +0100

    import companyHome component

--- app/(CompanyDashboard)/company-dashboard/page.tsx
+++ app/(CompanyDashboard)/company-dashboard/page.tsx
@@ -1,9 +1,9 @@
+import CompanyHome from "@/app/components/company-components/CompanyHome";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="pt-10">This is Company Dashboard Page</p>
-
-      <p>Welcome to Geomatic Connect</p>
+    <main className="min-h-screen p-6 lg:p-12 xl:p-20">
+      <CompanyHome />
     </main>
   );
 }
