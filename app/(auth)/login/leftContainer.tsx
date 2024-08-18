8f030a7a6998ce7d0409c29b5d3e6356dc0e19dd
geodevcode <geodevinstitute@gmail.com>
2024-08-18 10:27:26 +0100

    lazy loading set for background image

--- app/(auth)/login/leftContainer.tsx
+++ app/(auth)/login/leftContainer.tsx
@@ -1,9 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
 
 export default function LeftContainer() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = "/images/image2.jpg";
+    img.onload = () => setIsLoading(false);
+  }, []);
+
   return (
     <div className="relative w-full hidden md:w-1/2 md:block">
-      {/* Background Image */}
-      <div className="absolute inset-0 bg-[url(/images/image2.jpg)] bg-center bg-no-repeat bg-cover"></div>
+      {/* ===== Background Image ===== */}
+      <div
+        className={`absolute inset-0 bg-center bg-no-repeat bg-cover ${
+          isLoading ? "bg-gray-200" : "bg-[url(/images/image2.jpg)]"
+        }`}
+      ></div>
 
       {/* =====Overlay with color and opacity==== */}
       <div className="absolute inset-0 bg-[#F1F4EA] opacity-80"></div>
@@ -17,4 +31,3 @@ export default function LeftContainer() {
     </div>
   );
 }
-
