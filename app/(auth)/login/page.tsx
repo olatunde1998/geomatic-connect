0a711391d57522073b10701a0653494418bae42d
geodevcode <geodevinstitute@gmail.com>
2024-08-19 14:51:37 +0100

    neccessary styling added

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import LoginHome from "./LoginHome";
 
 export default function Login() {
   return (
-    <div className="text-[#1F4D36] md:flex justify-between h-screen ">
+    <div className="text-[#1F4D36] md:flex justify-between h-screen overflow-y-hidden">
       <LeftContainer />
       <LoginHome />
     </div>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import SignUpHome from "./SignupHome";
 
 export default function SignUp() {
   return (
-    <div className="text-[#1F4D36] md:flex justify-between h-screen ">
+    <div className="text-[#1F4D36] md:flex justify-between h-screen overflow-y-hidden">
       <LeftContainer />
       <SignUpHome />
     </div>
