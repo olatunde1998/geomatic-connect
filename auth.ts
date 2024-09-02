e218420e567d1576a769097f42cb4ec5578bafb2
geodevcode <geodevinstitute@gmail.com>
2024-09-02 17:49:31 +0100

    auth.ts file updated and email/password credential added

--- auth.ts
+++ auth.ts
@@ -1,53 +1,64 @@
-import GoogleProvider from "next-auth/providers/google"
-import GithubProvider from "next-auth/providers/github";
+import Google from "next-auth/providers/google"
+// import GithubProvider from "next-auth/providers/github";
 import NextAuth from "next-auth"
-// import credentials from "next-auth/providers/credentials";
+import credentials from "next-auth/providers/credentials";
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
     }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-    // credentials({
-    //     name: "Credentials",
-    //     async authorize(credentials) {
-    //       const response = await fetch(
-    //         `${process.env.NEXT_PUBLIC_BASEURL}/users/login`,
-    //         {
-    //           method: "POST",
-    //           body: JSON.stringify(credentials),
-    //           headers: { "Content-Type": "application/json" },
-    //         }
-    //       );
-    //       const user = await response.json();
-    //       // If no error and we have user data, return it
-    //       if (response.ok && user) {
-    //         return user;
-    //       }
-    //       // Return null if user data could not be retrieved
-    //       return null;
-    //     },
-    //   }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_CLIENT_ID as string,
+    //   clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    // }),
+    credentials({
+        name: "Credentials",
+        async authorize(credentials) {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_BASEURL}/auth/login`,
+            {
+              method: "POST",
+              body: JSON.stringify(credentials),
+              headers: { "Content-Type": "application/json" },
+            }
+          );
+          const user = await response.json();
+          console.log(user, "this is user here ===")
+          // If no error and we have user data, return it
+          if (response.ok && user?.data) {
+            return user.data;
+          }
+          // Return null if user data could not be retrieved
+          return null;
+        },
+      }),
   ],
   secret: process.env.AUTH_SECRET,
   callbacks: {
-    jwt({ token, user }) {
-      if (user) { // User is available during sign-in
-        token.id = user.id
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    jwt({ token, user, trigger, session }) {
+      if (user) {
+          token._id = user._id as string;
+          token.role = user.role as string;
+          token.username = user.username as string;
+
       }
-      return token
-    },
+      if (trigger === "update" && session) {
+          token = { ...token, ...session };
+      }
+      return token;
+  },
     session({ session, token }) {
-      console.log(session, "session is here ===== pls check==")
-      // session.user.id = token.id
-      return session
-    },
+      session.user._id = token._id;
+      session.user.role = token.role;
+      session.user.name = token.username;
+      return session;
+  }
   },
  
   pages: {
