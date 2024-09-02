5fa7f36400ed0f0fb86aad859cb758e6f4333942
geodevcode <geodevinstitute@gmail.com>
2024-09-02 17:47:36 +0100

    next-auth.d.ts file created

---
+++ types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import "next-auth";
+import "next-auth/jwt";
+
+declare module "next-auth" {
+    interface User {
+        _id: string
+        role: string
+        username: string
+    }
+    interface Session {
+        user: User
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        _id: string
+        role: string
+        username: string
+    }
+}
