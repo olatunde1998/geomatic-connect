dc009719e3a967c7c2a816f54b8d881391f621ee
geodevcode <geodevinstitute@gmail.com>
2024-09-04 11:37:09 +0100

    session imported

--- app/(StudentDashboard)/student-dashboard/layout.tsx
+++ app/(StudentDashboard)/student-dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/app/components/navbar/Navbar";
+import { auth } from "@/auth";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -9,15 +10,16 @@ export const metadata: Metadata = {
   description: "Geomatic Connect Application Dashboard",
 };
 
-export default function StudentLayout({
+export default async function StudentLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <div className={inter.className}>
       <div className="bg-[#F8F9FC]">
-        <Navbar />
+        <Navbar session={session} />
         {children}
       </div>
     </div>
