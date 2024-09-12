c4b780bf2f0974f1e5f028faacd9584fbaa22e6d
geodevcode <geodevinstitute@gmail.com>
2024-09-12 14:02:25 +0100

    database.ts updated

--- utils/database.ts
+++ utils/database.ts
@@ -1,12 +1,31 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+    if (isConnected) {
+        console.log('MongoDB is already connected');
+        return;
+    }
     try {
         await mongoose.connect(process.env.MONGO_URI as string, {
             dbName: "node-auth-v1-db",
         });
+        isConnected = true; 
         console.log('MongoDB connected');
     } catch (error) {
         console.log(error);
     }
 };
+
+
+// export const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI as string, {
+//             dbName: "node-auth-v1-db",
+//         });
+//         console.log('MongoDB connected');
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
