ee1d61bc13ffa400a17313f01f73d3946bfd2eac
geodevcode <geodevinstitute@gmail.com>
2024-10-13 13:45:46 +0100

    searchParams added

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,11 +1,12 @@
 import ResetPassword from "@/app/components/auth-components/ResetPassword";
 import LeftContainer from "./leftContainer";
 
-export default async function ResetPasswordPage() {
+export default async function ResetPasswordPage({ searchParams }: { searchParams: any }) {
+  const resetPasswordToken = searchParams?.resetToken;
   return (
     <div className="text-[#1F4D36] md:flex justify-between h-screen overflow-y-hidden">
       <LeftContainer />
-      <ResetPassword />
+      <ResetPassword token={resetPasswordToken} />
     </div>
   );
 }
