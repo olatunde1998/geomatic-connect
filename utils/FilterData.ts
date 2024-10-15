38b585820f4555dbbe953daf50f14b30da1acc3a
geodevcode <geodevinstitute@gmail.com>
2024-10-15 11:00:25 +0100

    filterData updated

--- utils/FilterData.ts
+++ utils/FilterData.ts
@@ -1,58 +1,89 @@
-    // Specialization data
+// Specialization data
 export const specializationData = [
-    { id: 1, label: "Cadastral Survey", value: "cadastral-survey" },
-    { id: 2, label: "Drone Survey", value: "drone-survey" },
-    { id: 3, label: "GIS & Remote Sensing", value: "GIS-remote-sensing" },
-    { id: 4, label: "Hydrographical Survey", value: "hydrographical-survey" },
-    { id: 5, label: "Topographical Survey", value: "topographical-survey" },
-  ];
+  { id: 1, label: "Cadastral Survey", value: "cadastral-survey" },
+  { id: 2, label: "Drone Survey", value: "drone-survey" },
+  { id: 3, label: "GIS & Remote Sensing", value: "GIS-remote-sensing" },
+  { id: 4, label: "Hydrographical Survey", value: "hydrographical-survey" },
+  { id: 5, label: "Topographical Survey", value: "topographical-survey" },
+];
 
-    // State data
-  export const stateData = [
-    { id: 1, label: "Abia", value: "abia" },
-    { id: 2, label: "Adamawa", value: "adamawa" },
-    { id: 3, label: "Akwa Ibom", value: "akwa-ibom" },
-    { id: 4, label: "Anambra", value: "anambra" },
-    { id: 5, label: "Bauchi", value: "bauchi" },
-    { id: 6, label: "Bayelsa", value: "bayelsa" },
-    { id: 7, label: "Benue", value: "benue" },
-    { id: 8, label: "Borno", value: "borno" },
-    { id: 9, label: "Cross River", value: "cross-river" },
-    { id: 10, label: "Delta", value: "delta" },
-    { id: 11, label: "Ebonyi", value: "ebonyi" },
-    { id: 12, label: "Edo", value: "edo" },
-    { id: 13, label: "Ekiti", value: "ekiti" },
-    { id: 14, label: "Enugu", value: "enugu" },
-    { id: 15, label: "FCT, Abuja", value: "FCT-Abuja" },
-    { id: 16, label: "Gombe", value: "gombe" },
-    { id: 17, label: "Imo", value: "imo" },
-    { id: 18, label: "Jigawa", value: "jigawa" },
-    { id: 19, label: "Kaduna", value: "kaduna" },
-    { id: 20, label: "Kano", value: "kano" },
-    { id: 21, label: "Katsina", value: "katsina" },
-    { id: 22, label: "Kebbi", value: "kebbi" },
-    { id: 23, label: "Kogi", value: "kogi" },
-    { id: 24, label: "Kwara", value: "kwara" },
-    { id: 25, label: "Lagos", value: "lagos" },
-    { id: 26, label: "Nasarawa", value: "nasarawa" },
-    { id: 27, label: "Niger", value: "niger" },
-    { id: 28, label: "Ogun", value: "ogun" },
-    { id: 29, label: "Ondo", value: "ondo" },
-    { id: 30, label: "Osun", value: "osun" },
-    { id: 31, label: "Oyo", value: "oyo" },
-    { id: 32, label: "Plateau", value: "Plateau" },
-    { id: 33, label: "Rivers", value: "Rivers" },
-    { id: 34, label: "Sokoto", value: "Sokoto" },
-    { id: 35, label: "Taraba", value: "Taraba" },
-    { id: 36, label: "Yobe", value: "yobe" },
-    { id: 37, label: "Zamfara", value: "zamfara" },
-  ];
+// State data
+export const stateData = [
+  { id: 1, label: "Abia", value: "abia" },
+  { id: 2, label: "Adamawa", value: "adamawa" },
+  { id: 3, label: "Akwa Ibom", value: "akwa-ibom" },
+  { id: 4, label: "Anambra", value: "anambra" },
+  { id: 5, label: "Bauchi", value: "bauchi" },
+  { id: 6, label: "Bayelsa", value: "bayelsa" },
+  { id: 7, label: "Benue", value: "benue" },
+  { id: 8, label: "Borno", value: "borno" },
+  { id: 9, label: "Cross River", value: "cross-river" },
+  { id: 10, label: "Delta", value: "delta" },
+  { id: 11, label: "Ebonyi", value: "ebonyi" },
+  { id: 12, label: "Edo", value: "edo" },
+  { id: 13, label: "Ekiti", value: "ekiti" },
+  { id: 14, label: "Enugu", value: "enugu" },
+  { id: 15, label: "FCT, Abuja", value: "FCT-Abuja" },
+  { id: 16, label: "Gombe", value: "gombe" },
+  { id: 17, label: "Imo", value: "imo" },
+  { id: 18, label: "Jigawa", value: "jigawa" },
+  { id: 19, label: "Kaduna", value: "kaduna" },
+  { id: 20, label: "Kano", value: "kano" },
+  { id: 21, label: "Katsina", value: "katsina" },
+  { id: 22, label: "Kebbi", value: "kebbi" },
+  { id: 23, label: "Kogi", value: "kogi" },
+  { id: 24, label: "Kwara", value: "kwara" },
+  { id: 25, label: "Lagos", value: "lagos" },
+  { id: 26, label: "Nasarawa", value: "nasarawa" },
+  { id: 27, label: "Niger", value: "niger" },
+  { id: 28, label: "Ogun", value: "ogun" },
+  { id: 29, label: "Ondo", value: "ondo" },
+  { id: 30, label: "Osun", value: "osun" },
+  { id: 31, label: "Oyo", value: "oyo" },
+  { id: 32, label: "Plateau", value: "Plateau" },
+  { id: 33, label: "Rivers", value: "Rivers" },
+  { id: 34, label: "Sokoto", value: "Sokoto" },
+  { id: 35, label: "Taraba", value: "Taraba" },
+  { id: 36, label: "Yobe", value: "yobe" },
+  { id: 37, label: "Zamfara", value: "zamfara" },
+];
 
+// State data
+export const institutionData = [
+  { id: 1, label: "Federal School of Surveying, Oyo", value: "fss-oyo" },
+  { id: 2, label: "Federal University of Tech, Minna", value: "futminna" },
+  { id: 3, label: "Federal University of Tech, Akure", value: "futa" },
+  { id: 4, label: "Ahmadu Bello University, Zaria", value: "abu-zaria" },
+  { id: 5, label: "Lead City University, Ibadan", value: "leadcity" },
+  { id: 6, label: "Nigeria Army University, Biu", value: "naub" },
+  { id: 7, label: "The Bells University of Tech, Ota", value: "bells" },
+  { id: 8, label: "Ajayi Crowther University", value: "acu" },
+  // { id: 9, label: "Cross River", value: "cross-river" },
+  // { id: 10, label: "Delta", value: "delta" },
+  // { id: 11, label: "Ebonyi", value: "ebonyi" },
+  // { id: 12, label: "Edo", value: "edo" },
+  // { id: 13, label: "Ekiti", value: "ekiti" },
+  // { id: 14, label: "Enugu", value: "enugu" },
+  // { id: 15, label: "FCT, Abuja", value: "FCT-Abuja" },
+  // { id: 16, label: "Gombe", value: "gombe" },
+  // { id: 17, label: "Imo", value: "imo" },
+  // { id: 18, label: "Jigawa", value: "jigawa" },
+  // { id: 19, label: "Kaduna", value: "kaduna" },
+  // { id: 20, label: "Kano", value: "kano" },
+];
 
-  // Track Period data
-  export const trackPeriodData = [
-    { id: 1, label: "Fast Track (3 days)", value: "3 days" },
-    { id: 2, label: "Standard Track (7 days)", value: "7 days" },
-    { id: 3, label: "Moderate Track (2 weeks)", value: "2 weeks" },
-    { id: 4, label: "Extended Track (3 weeks)", value: "3-weeks" },
-  ];
+// Track Period data
+export const trackPeriodData = [
+  { id: 1, label: "Fast Track (3 days)", value: "3 days" },
+  { id: 2, label: "Standard Track (7 days)", value: "7 days" },
+  { id: 3, label: "Moderate Track (2 weeks)", value: "2 weeks" },
+  { id: 4, label: "Extended Track (3 weeks)", value: "3-weeks" },
+];
+
+// Purpose of Request data
+export const purposeOfRequestData = [
+  { id: 1, label: "Professional Training", value: "professional-training" },
+  { id: 2, label: "Industrial Training (IT)", value: "IT-Training" },
+  { id: 3, label: "SIWES Training", value: "siwes-training" },
+  { id: 4, label: "Internship Training", value: "internship-training" },
+];
