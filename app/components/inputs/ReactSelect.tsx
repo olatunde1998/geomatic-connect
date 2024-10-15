b7491519320a4b49c98f427d39f276cec4607e0f
geodevcode <geodevinstitute@gmail.com>
2024-10-15 10:59:47 +0100

    ReactSelect updated

--- app/components/inputs/ReactSelect.tsx
+++ app/components/inputs/ReactSelect.tsx
@@ -1,13 +1,17 @@
 import Select from "react-select";
 
-const customStyles = (width: string) => ({
+const customStyles = (padding: string, backgroundColor: string, borderRadius: string, border: string ) => ({
   control: (provided: any, state: any) => ({
     ...provided,
-    width: width,
+    width: "100%",
     borderShadow: "none",
     textAlign: "left",
-    padding: "10px",
+    backgroundColor, 
+    padding,
     boxShadow: provided.boxShadow,
+    borderRadius,
+    border,
+    fontSize: "14px",
     borderColor: state.isFocused ? "transparent" : provided.borderColor,
   }),
   option: (provided: any, state: any) => ({
@@ -19,13 +23,16 @@ const customStyles = (width: string) => ({
   }),
 });
 
-export default function ReactSelect({ options, setOptionPicked, placeholder, width = "220px" }: any) {
+export default function ReactSelect({ options, placeholder, onChange, padding="10px", backgroundColor = "#ffffff", borderRadius="none", border="1px solid #cbd5e1 " }: any) {
   return (
     <Select
       options={options}
-      styles={customStyles(width)}
-      onChange={(option) => setOptionPicked(option)}
+      styles={customStyles(padding, backgroundColor, borderRadius, border)}
+      onChange={(option) => {
+        onChange(option); 
+      }}
       placeholder={placeholder}
+      
     />
   );
 }
