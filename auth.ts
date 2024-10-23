5c1be098304fee0e32ed493a88a0590d123f86c3
geodevcode <geodevinstitute@gmail.com>
2024-10-23 06:55:17 +0100

    refactored nextauth setup

--- auth.ts
+++ auth.ts
@@ -1,70 +1,71 @@
 import credentials from "next-auth/providers/credentials";
-import Google from "next-auth/providers/google"
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+// import Google from "next-auth/providers/google";
 // import GithubProvider from "next-auth/providers/github";
-
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+    // Google({
+    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    // }),
     // GithubProvider({
     //   clientId: process.env.GITHUB_CLIENT_ID as string,
     //   clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     // }),
     credentials({
-        name: "Credentials",
-        async authorize(credentials) {
-          const response = await fetch(
-            `${process.env.NEXT_PUBLIC_BASEURL}/auth/login`,
-            {
-              method: "POST",
-              body: JSON.stringify(credentials),
-              headers: { "Content-Type": "application/json" },
-            }
-          );
-          const user = await response.json();
-          // If no error and we have user data, return it
-          if (response.ok && user?.data) {
-            return user.data;
+      name: "Credentials",
+      async authorize(credentials) {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASEURL}/auth/login`,
+          {
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: { "Content-Type": "application/json" },
           }
-          // Return null if user data could not be retrieved
-          return null;
-        },
-      }),
+        );
+        const user = await response.json();
+        // If no error and we have user data, return it
+        if (response.ok && user?.data) {
+          return {
+            _id: user.data._id,
+            role: user.data.role,
+            email: user.data.email,
+            token: user.token,
+          };
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
   ],
   secret: process.env.AUTH_SECRET,
 
   callbacks: {
-    async redirect({ url, baseUrl }) {
+    async redirect({ baseUrl }) {
       return baseUrl;
     },
-    jwt({ token, user, trigger, session }) {
+    jwt({ token, user }) {
       if (user) {
-          token._id = user._id as string;
-          token.role = user.role as string;
-          token.username = user.username as string;
-
-      }
-      if (trigger === "update" && session) {
-          token = { ...token, ...session };
+        token._id = user._id as string;
+        token.role = user.role as string;
+        token.email = user.email as string;
+        token.role = user.role as string;
+        token.token = user.token as string;
       }
       return token;
-  },
+    },
     async session({ session, token }) {
-      console.log(session, "this is session here")
-      session.user._id = token._id ,
+      session.user.token = token.token;
+      session.user._id = token._id;
       session.user.role = token.role;
-      session.user.name = token.username;
+      session.user.token = token.token;
       return session;
+    },
   },
 
-  },
- 
   pages: {
     signIn: "/login",
-    error: '/not-found',
+    error: "/not-found",
   },
-})
+});
