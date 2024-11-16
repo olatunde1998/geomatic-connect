ffba4ea32e1469d67b3d426ab63af833367e6626
geodevcode <geodevinstitute@gmail.com>
2024-11-16 16:33:50 +0100

    billingPage updated

--- app/(AdminDashboard)/admin-dashboard/billing/page.tsx
+++ app/(AdminDashboard)/admin-dashboard/billing/page.tsx
@@ -1,7 +1,28 @@
+import TransactionList from "@/app/components/admin-components/TransactionList";
+import StatisticsCard from "@/app/components/cards/StatisticsCard";
+import { auth } from "@/auth";
+
 export default async function BillingPage() {
+  const session = await auth();
+  const token = session?.user?.token;
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="pt-10">This is Billing (Payment History page)</p>
-    </div>
+    <main className="flex min-h-screen flex-col pt-32">
+      <div className="w-full">
+        <p className="text-gray-600 text-lg font-semibold">
+          Manage Transactions
+        </p>
+        <p className="text-sm text-gray-500 font-normal">
+          Administer transactions histories within the platform.
+        </p>
+      </div>
+      <div className="my-8 grid space-y-6 lg:space-y-0 lg:grid-cols-3 lg:gap-3 xl:grid-cols-4 xl:gap-6">
+        <StatisticsCard title={"Total Users"} value={10} />
+        <StatisticsCard title={"Total Requests"} value={20} />
+        <StatisticsCard title={"Response Rate (%)"} value={40} />
+        <StatisticsCard title={"Average Completion Time"} value={3} />
+      </div>
+
+      <TransactionList token={token} />
+    </main>
   );
 }
