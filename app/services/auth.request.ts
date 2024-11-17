3654df7ce02a0b43715835d0624014f8d2501c80
geodevcode <geodevinstitute@gmail.com>
2024-11-17 13:26:06 +0100

    prettier formatter use and registerwithgoogle social auth request added

--- app/services/auth.request.ts
+++ app/services/auth.request.ts
@@ -2,38 +2,45 @@ import axios from "axios";
 
 // CREATE NEW USER REQUEST (REGISTER USER)
 export const RegisterRequest = async (body: any) => {
-    try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_BASEURL}/auth/register`, body, {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASEURL}/auth/register`,
+      body,
+      {
         headers: {
           Accept: "application/vnd.connect.v1+json",
           "Content-Type": "application/json",
         },
-      });
-      const data = response.data;
-      console.log(data, "data is here");
-      if (!data) return;
-      return data;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
+      }
+    );
+    const data = response.data;
+    console.log(data, "data is here");
+    if (!data) return;
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-  // REGISTER WITH GOOGLE (SIGN UP)
+// REGISTER WITH GOOGLE (SIGN UP)
 export const RegisterWithGoogleRequest = async () => {
-    // const response = await axios.get(`${process.env.NEXT_PUBLIC_BASEURL}/auth/google`, {
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_BASEURL}/auth/google/redirect`, {
+  // const response = await axios.get(`${process.env.NEXT_PUBLIC_BASEURL}/auth/google`, {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASEURL}/auth/google/redirect`,
+    {
       maxBodyLength: Infinity,
       headers: {
         Accept: "application/vnd.connect.v1+json",
         "Content-Type": "application/json",
       },
-    });
-    const data = await response.data;
-    return data;
-  };
+    }
+  );
+  const data = await response.data;
+  return data;
+};
 
-  // EMAIL VERIFICATION REQUEST
+// EMAIL VERIFICATION REQUEST
 export const VerifyEmailRequest = async (body: any) => {
   try {
     const response = await axios.post(
@@ -56,48 +63,62 @@ export const VerifyEmailRequest = async (body: any) => {
   }
 };
 
-  // FORGOT PASSWORD REQUEST
-  export const ForgotPasswordRequest = async (body: any) => {
-    try {
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_BASEURL}/auth/forgot-password`,
-        body,
-        {
-          headers: {
-            Accept: "application/vnd.connect.v1+json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = response.data;
-      console.log(data, "data is here");
-      if (!data) return;
-      return data;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
+// FORGOT PASSWORD REQUEST
+export const ForgotPasswordRequest = async (body: any) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASEURL}/auth/forgot-password`,
+      body,
+      {
+        headers: {
+          Accept: "application/vnd.connect.v1+json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = response.data;
+    console.log(data, "data is here");
+    if (!data) return;
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-  // RESET PASSWORD REQUEST
-  export const ResetPasswordRequest = async (body: any, token: string) => {
-    try {
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_BASEURL}/auth/reset-password/${token}`,
-        body,
-        {
-          headers: {
-            Accept: "application/vnd.connect.v1+json",
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = response.data;
-      console.log(data, "data is here");
-      if (!data) return;
-      return data;
-    } catch (error) {
-      console.error(error);
-      throw error;
+// RESET PASSWORD REQUEST
+export const ResetPasswordRequest = async (body: any, token: string) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASEURL}/auth/reset-password/${token}`,
+      body,
+      {
+        headers: {
+          Accept: "application/vnd.connect.v1+json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = response.data;
+    console.log(data, "data is here");
+    if (!data) return;
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// SOCIAL AUTH
+export const RegisterWithGoogle = async () => {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASEURL}/auth/google`, // Match your backend route
+    {
+      maxBodyLength: Infinity,
+      headers: {
+        Accept: "application/vnd.connect.v1+json",
+      },
     }
-  };
+  );
+  return response.data;
+};
