47ed3dab436b6380bb7ae2e3d18bac8f9f67a1e3
geodevcode <geodevinstitute@gmail.com>
2024-11-17 13:24:53 +0100

    necessary styling added

--- app/(AdminDashboard)/admin-dashboard/layout.tsx
+++ app/(AdminDashboard)/admin-dashboard/layout.tsx
@@ -22,11 +22,11 @@ export default async function AdminLayout({
       <div className={inter.className}>
         <DashboardNavBar session={session} />
         <div className="flex flex-col space-y-6">
-          <div className="grid flex-1 gap-12">
-            <div className="hidden w-[200px] fixed flex-col md:block border-r border-accent ml-10 pt-32 pr-2 min-h-screen">
+          <div className="grid flex-1 gap-12 ">
+            <div className="hidden w-[200px] md:w-[160px] lg:w-[160px] xl:w-[200px] fixed flex-col md:block border-r ml-6 pr-2 lg:ml-10 pt-32 min-h-screen">
               <AdminSidebar />
             </div>
-            <main className="px-6 md:pl-72 md:pr-12">
+            <main className="px-6 md:pl-52 lg:pl-60 xl:pl-72 md:pr-12 overflow-x-auto">
               {children}
             </main>
           </div>
