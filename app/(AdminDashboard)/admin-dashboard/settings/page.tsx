db7a3bbe3fb3955eb5d3e6c8f3c30d1cf065a25c
geodevcode <geodevinstitute@gmail.com>
2024-11-18 13:34:23 +0100

    styling added

--- app/(AdminDashboard)/admin-dashboard/settings/page.tsx
+++ app/(AdminDashboard)/admin-dashboard/settings/page.tsx
@@ -1,16 +1,20 @@
 import { auth } from "@/auth";
+import Settings from "@/app/components/admin-components/Settings";
 
 export default async function SettingsPage() {
   const session = await auth();
   const token = session?.user?.token;
+  const userId = session?.user?._id;
+
   return (
-    <main className="flex min-h-screen flex-col pt-32">
-      <div className="w-full">
-        <p className="text-gray-600 text-lg font-semibold">Settings</p>
-        <p className="text-sm text-gray-500 font-normal">
-          Manage the settings of your account
-        </p>
+    <main className="min-h-screen  pt-32  xl:p-2 lg:pt-32 xl:pt-32 font-sans text-md">
+      <div className="w-full font-sans text-md ">
+        <p className="text-2xl font-bold mb-2">Settings</p>
+        <p className="text-gray-500">Manage the settings of your account</p>
       </div>
+      <section className="h-fit border border-accent-primary mt-8 p-6 rounded-md">
+        <Settings token={token} userId={userId} />
+      </section>
     </main>
   );
 }
