2fa5252ca731b4079d6852343b7be93e6d343ee3
geodevcode <geodevinstitute@gmail.com>
2024-11-20 08:48:37 +0100

    image, document  upload implementation done

--- app/components/student-components/Settings.tsx
+++ app/components/student-components/Settings.tsx
@@ -35,7 +35,12 @@ const schema = yup.object().shape({
 
 export default function Settings({ token, userId }: SettingsProps) {
   const [userImage, setUserImage] = useState<string | undefined>(undefined);
+  const [userDocument, setUserDocument] = useState<string | undefined>(
+    undefined
+  );
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [selectedDocument, setSelectedDocument] = useState<File | null>(null);
+
   const [isUpdating, setIsUpdating] = useState<boolean>(false);
   const queryClient = useQueryClient();
 
@@ -76,6 +81,33 @@ export default function Settings({ token, userId }: SettingsProps) {
       ) {
         setUserImage(URL.createObjectURL(files[0]));
         setSelectedFile(file);
+      } else {
+        toast.error("Unsupported file type. Please upload a JPG, PNG, or JPEG");
+      }
+    }
+  };
+
+  // Uploading document logic
+  const handleDocumentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files[0]) {
+      const file = files[0];
+      const fileType = file.type;
+
+      console.log(fileType, "this is the file type");
+
+      if (
+        fileType === "image/jpg" ||
+        fileType === "image/png" ||
+        fileType === "image/jpeg" ||
+        fileType === "application/pdf"
+      ) {
+        setUserDocument(URL.createObjectURL(file));
+        setSelectedDocument(file);
+      } else {
+        toast.error(
+          "Unsupported file type. Please upload a JPG, PNG, JPEG, or PDF."
+        );
       }
     }
   };
@@ -83,43 +115,29 @@ export default function Settings({ token, userId }: SettingsProps) {
   // Submit handler for the form
   const onSubmitHandler = async (data: any) => {
     setIsUpdating(true);
-    // console.log(selectedFile, "this is the file selected===");
-    // if (selectedFile) {
-    //   const formData = new FormData();
-    //   formData.append("avatar", selectedFile);
-    //   formData.append("firstName", data?.firstName);
-    //   formData.append("lastName", data?.lastName);
-    //   formData.append("email", data?.email);
-    //   formData.append("password", data?.password);
+    console.log(selectedFile, "this is the file selected===");
+    console.log(selectedDocument, "this is the document selected===");
+    if (selectedFile && selectedDocument) {
+      const formData = new FormData();
+      formData.append("fullName", data?.fullName);
+      formData.append("email", data?.email);
+      formData.append("phoneNumber", data?.mobileNumber);
+      formData.append("avatarImage", selectedFile);
+      formData.append("documentFile", selectedDocument);
 
-    //   const body = formData;
-    //   try {
-    //     await UpdateUsersProfileRequest(userId, token, body);
-    //     toast.success("Settings Updated Successfully");
-    //     queryClient.invalidateQueries({ queryKey: ["getUserProfileApi"] });
-    //   } catch (error: any) {
-    //     console.log(error?.response?.data?.message);
-    //     toast.error(error?.response?.data?.message);
-    //   } finally {
-    //     setIsUpdating(false);
-    //   }
-    // }
-
-    const body = {
-      fullName: data?.fullName,
-      email: data?.email,
-      phoneNumber: data?.mobileNumber,
-    };
-    try {
-      const response = await UpdateUserProfileRequest(userId, token, body);
-      toast.success(response?.message);
-      queryClient.invalidateQueries({ queryKey: ["getUserProfileApi"] });
-      queryClient.invalidateQueries({ queryKey: ["getUsersApi"] });
-    } catch (error: any) {
-      console.log(error?.response?.data?.message);
-      toast.error(error?.response?.message);
-    } finally {
-      setIsUpdating(false);
+      const body = formData;
+      try {
+        const response = await UpdateUserProfileRequest(userId, token, body);
+        console.log(response, "this is response here====");
+        toast.success(response?.message);
+        queryClient.invalidateQueries({ queryKey: ["getUserProfileApi"] });
+        queryClient.invalidateQueries({ queryKey: ["getUsersApi"] });
+      } catch (error: any) {
+        console.log(error?.response?.data?.message);
+        toast.error(error?.response?.message);
+      } finally {
+        setIsUpdating(false);
+      }
     }
   };
   return (
@@ -174,7 +192,7 @@ export default function Settings({ token, userId }: SettingsProps) {
             <p className="">Profile picture</p>
             <div className="flex items-center justify-center space-x-2 md:space-x-6 bg-white rounded-2xl  border-[0.6px] border-slate-300 mt-4 cursor-pointer">
               <label
-                htmlFor="fileInput"
+                htmlFor="avatarInput"
                 className="w-full p-3 flex  justify-between tracking-wide cursor-pointer"
               >
                 <div className="flex w-full items-center justify-between gap-2">
@@ -182,16 +200,26 @@ export default function Settings({ token, userId }: SettingsProps) {
                   <input
                     type="file"
                     name="user_Image"
-                    id="fileInput"
+                    id="avatarInput"
                     accept=".png,  .jpg, .jpeg"
                     className="hidden input-field"
                     onChange={handleFileChange}
                   />
 
-                  {userImage && (
+                  {userImage ? (
                     <div className="border-2 border-slate-800 rounded-full relative mx-auto w-[45px]">
                       <Image
                         src={userImage}
+                        alt="user avatar"
+                        width={100}
+                        height={100}
+                        className="rounded-full w-[45px] h-[35px]"
+                      />
+                    </div>
+                  ) : (
+                    <div className="border-2 border-slate-800 rounded-full relative mx-auto w-[45px]">
+                      <Image
+                        src={userProfileData?.data?.avatarImage}
                         alt="user avatar"
                         width={100}
                         height={100}
@@ -202,6 +230,52 @@ export default function Settings({ token, userId }: SettingsProps) {
                 </div>
               </label>
             </div>
+          </div>
+        </section>
+
+        {/* =====Documents  ===== */}
+        <section>
+          <div className="border-[0.5px] border-slate-300 px-4 pt-3 pb-6 md:px-10 md:pt-6 md:pb-6 rounded-xl bg-white max-w-[540px] mt-6">
+            <p className="">User Document</p>
+            <div className="flex items-center justify-center space-x-2 md:space-x-6 bg-white rounded-2xl  border-[0.6px] border-slate-300 mt-4 cursor-pointer">
+              <label
+                htmlFor="documentInput"
+                className="w-full p-3 flex  justify-between tracking-wide cursor-pointer"
+              >
+                <div className="flex w-full items-center justify-between gap-2">
+                  <p className="w-full text-center">Upload docs</p>
+                  <input
+                    type="file"
+                    name="document_Image"
+                    id="documentInput"
+                    accept=".png, .jpg, .jpeg, .pdf, .doc, .docx"
+                    className="hidden input-field"
+                    onChange={handleDocumentChange}
+                  />
+
+                  {userDocument && (
+                    <div className="border-2 border-slate-800 rounded-full relative mx-auto w-[45px]">
+                      <Image
+                        src={userDocument}
+                        alt="user document"
+                        width={100}
+                        height={100}
+                        className="rounded-full w-[45px] h-[35px]"
+                      />
+                    </div>
+                  )}
+                </div>
+              </label>
+            </div>
+
+            <a
+              href={`${userProfileData?.data?.documentFile}`}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="font-semibold mt-2 w-full text-right inline-block text-[#33A852] underline"
+            >
+              View Document
+            </a>
           </div>
         </section>
 
