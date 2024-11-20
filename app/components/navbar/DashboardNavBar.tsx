060f4825ad3847db738726f8fd8a3bbfdd48ad6a
geodevcode <geodevinstitute@gmail.com>
2024-11-20 08:49:23 +0100

    avatarImage rendered into Image src

--- app/components/navbar/DashboardNavBar.tsx
+++ app/components/navbar/DashboardNavBar.tsx
@@ -115,14 +115,14 @@ export default function DashboardNavBar({ session }: { session: any }) {
                   className="flex justify-start"
                 >
                   <div className="relative cursor-pointer">
-                    {!GeomaticLogo ? (
+                    {!userData?.data?.avatarImage ? (
                       <div className="w-[50px] h-[50px] border-[1.3px] border-slate-200 items-center justify-center flex rounded-full text-[20px] bg-[#524A4C] text-white font-bold">
                         {userData?.data?.fullName?.charAt(0)?.toUpperCase() ??
                           "N/A"}
                       </div>
                     ) : (
                       <Image
-                        src={GeomaticLogo}
+                        src={userData?.data?.avatarImage}
                         alt="user avatar pics"
                         width={42}
                         height={42}
