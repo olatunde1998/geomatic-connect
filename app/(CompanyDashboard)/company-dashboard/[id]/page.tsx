a4241650d8616dd622ad486808ffd9753633be80
geodevcode <geodevinstitute@gmail.com>
2024-11-23 01:50:21 +0100

    neccessary styling added

--- app/(CompanyDashboard)/company-dashboard/[id]/page.tsx
+++ app/(CompanyDashboard)/company-dashboard/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function StudentDetailsPage({ params }: { params: any }) {
   }
   return (
     <>
-      <main className="min-h-screen p-6 lg:p-12 xl:p-20">
+      <main className="min-h-screen p-6 lg:p-12 xl:p-20 xl:px-2 xl:pl-16">
         <StudentDetails studentId={studentId} session={session} />
       </main>
     </>
