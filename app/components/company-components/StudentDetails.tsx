82305a048ac3ea3165a9d9236df8686135b96aad
geodevcode <geodevinstitute@gmail.com>
2024-11-23 01:50:48 +0100

    studentDetails refactored

--- app/components/company-components/StudentDetails.tsx
+++ app/components/company-components/StudentDetails.tsx
@@ -1,9 +1,4 @@
 "use client";
-import { useState } from "react";
-import StudentProfile from "./StudentProfile";
-import StudentProject from "./StudentProject";
-import StudentInstitution from "./StudentInstitution";
-import DetailsTab from "./DetailsTab";
 
 interface StudentDetailsProps {
   studentId?: any;
@@ -14,46 +9,16 @@ export default function StudentDetails({
   studentId,
   session,
 }: StudentDetailsProps) {
-  const [selectedTab, setSelectedTab] = useState("Profile");
   return (
     <>
       <div className="mt-24 mb-10 items-center justify-between bg-[#ECF1F7] lg:flex p-4  lg:my-20 xl:my-10">
         This is Student Id here === {studentId}
         {/*=====Complete  Profile Tabs ====== */}
-        <div>
-          <DetailsTab
-            selectedTab={selectedTab}
-            setSelectedTab={setSelectedTab}
-          />
-        </div>
       </div>
       {/* ================Body section ===========  */}
       <section className="mt-8">
-        {/* ==== Profile ====*/}
         <div>
-          {selectedTab === "Profile" && (
-            <section>
-              <StudentProfile />
-            </section>
-          )}
-        </div>
-
-        {/* ==== Project ====*/}
-        <div>
-          {selectedTab === "Project" && (
-            <section>
-              <StudentProject />
-            </section>
-          )}
-        </div>
-
-        {/* ==== Institution ====*/}
-        <div>
-          {selectedTab === "Institution" && (
-            <section>
-              <StudentInstitution />
-            </section>
-          )}
+          <p>This is Student Profile Tab</p>
         </div>
       </section>
     </>
