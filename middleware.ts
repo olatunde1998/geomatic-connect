26192293239e8835082953ded42beba52957ae53
geodevcode <geodevinstitute@gmail.com>
2024-11-26 08:21:11 +0100

    protected route added

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware } from "@/auth"
+export { auth as middleware } from "@/auth";
+
+export const config = {
+  matcher: [
+    "/admin-dashboard",
+    "/company-dashboard",
+    "/student-dashboard",
+    "/admin-dashboard/:path*",
+    "/company-dashboard/:path*",
+    "/student-dashboard/:path*",
+  ],
+};
