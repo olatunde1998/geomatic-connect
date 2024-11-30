43b1125ebf5a518d2c408cff2cff20234de37a04
geodevcode <geodevinstitute@gmail.com>
2024-11-30 11:01:21 +0100

    RequestsHome refactored

--- app/(AdminDashboard)/admin-dashboard/requests/page.tsx
+++ app/(AdminDashboard)/admin-dashboard/requests/page.tsx
@@ -1,6 +1,5 @@
-import RequestsList from "@/app/components/admin-components/RequestsList";
-import StatisticsCard from "@/app/components/cards/StatisticsCard";
 import { auth } from "@/auth";
+import RequestsHome from "@/app/components/admin-components/RequestsHome";
 import { redirect } from "next/navigation";
 
 export default async function RequestsPage() {
@@ -8,23 +7,8 @@ export default async function RequestsPage() {
   const token = session?.user?.token;
   if (!session?.user) redirect("/login");
   return (
-    <main className="flex min-h-screen flex-col pt-32">
-      <div className="w-full">
-        <p className="text-gray-600 text-lg font-semibold">
-          Manage Notifications
-        </p>
-        <p className="text-sm text-gray-500 font-normal">
-          Administer user requests and notifications within the platform.
-        </p>
-      </div>
-      <div className="my-8 grid space-y-6 lg:space-y-0 lg:grid-cols-3 lg:gap-3 xl:grid-cols-4 xl:gap-6">
-        <StatisticsCard title={"Total Requests"} value={40} />
-        <StatisticsCard title={"Total Approved"} value={10} />
-        <StatisticsCard title={"Total Declined"} value={20} />
-        <StatisticsCard title={"Total Sent to Companies"} value={10} />
-      </div>
-
-      <RequestsList token={token} />
-    </main>
+    <div>
+      <RequestsHome token={token} />
+    </div>
   );
 }
