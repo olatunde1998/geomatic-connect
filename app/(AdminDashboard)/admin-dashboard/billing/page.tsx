393320304395ce38123205ca725e294c5a243331
geodevcode <geodevinstitute@gmail.com>
2024-11-30 11:45:04 +0100

    BilligPage refactored

--- app/(AdminDashboard)/admin-dashboard/billing/page.tsx
+++ app/(AdminDashboard)/admin-dashboard/billing/page.tsx
@@ -1,6 +1,5 @@
-import TransactionList from "@/app/components/admin-components/TransactionList";
-import StatisticsCard from "@/app/components/cards/StatisticsCard";
 import { auth } from "@/auth";
+import BillingHome from "@/app/components/admin-components/BillingHome";
 import { redirect } from "next/navigation";
 
 export default async function BillingPage() {
@@ -8,23 +7,8 @@ export default async function BillingPage() {
   const token = session?.user?.token;
   if (!session?.user) redirect("/login");
   return (
-    <main className="flex min-h-screen flex-col pt-32">
-      <div className="w-full">
-        <p className="text-gray-600 text-lg font-semibold">
-          Manage Transactions
-        </p>
-        <p className="text-sm text-gray-500 font-normal">
-          Administer transactions histories within the platform.
-        </p>
-      </div>
-      <div className="my-8 grid space-y-6 lg:space-y-0 lg:grid-cols-3 lg:gap-3 xl:grid-cols-4 xl:gap-6">
-        <StatisticsCard title={"Total Transaction"} value={30} />
-        <StatisticsCard title={"Success Transaction"} value={20} />
-        <StatisticsCard title={"Failed Transaction"} value={6} />
-        <StatisticsCard title={"Abandoned Transaction"} value={4} />
-      </div>
-
-      <TransactionList token={token} />
-    </main>
+    <div>
+      <BillingHome token={token} />
+    </div>
   );
 }
