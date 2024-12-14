4b823baf2e7d1ef1bfb61f4998ad08db8ed9925f
geodevcode <geodevinstitute@gmail.com>
2024-12-14 14:01:08 +0100

    neccesary styling added

--- app/components/cards/TestimonialCard.tsx
+++ app/components/cards/TestimonialCard.tsx
@@ -10,7 +10,7 @@ export default function TestimonialCard() {
   return (
     <>
       <div>
-        <div className="w-[280px] md:w-[430px] px-6 py-8 md:py-16 md:px-12 border border-slate-300 bg-white rounded-xl">
+        <div className="w-[280px] md:w-[430px] px-6 py-8 md:py-12 md:px-12 border border-slate-300 bg-[#F2F6F6] rounded-xl">
           <div className="flex gap-3 ">
             <div>
               <Image
@@ -19,7 +19,7 @@ export default function TestimonialCard() {
                 width={100}
                 height={100}
                 priority
-                className="w-[100px] h-[100px] md:w-[120px] md:h-[120px] object-cover rounded-full"
+                className="w-[100px] h-[100px] md:w-[100px] md:h-[100px] object-cover rounded-full"
               />
             </div>
             <div className="mt-2">
@@ -40,9 +40,9 @@ export default function TestimonialCard() {
             </div>
           </div>
 
-          <p className="mt-10 text-base md:text-lg text-gray-700 font-light leading-[28px]">
+          <p className="mt-10 text-base md:text-lg text-gray-500 font-light leading-[28px]">
             “The services are totally great! they offered many options for me to
-            stay that suit my budget. Also, the room is well-furnished.”
+            stay that suit my budget. Also, the connection is outstanding.”
           </p>
         </div>
       </div>
