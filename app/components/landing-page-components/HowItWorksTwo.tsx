c67e2b0c0c91e34fb7c142362626c1a2254625b6
geodevcode <geodevinstitute@gmail.com>
2024-12-14 14:01:47 +0100

    HowitWorks altenative created

---
+++ app/components/landing-page-components/HowItWorksTwo.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+
+export default function HowItWorksTwo() {
+  return (
+    <main className="py-10 xl:px-6 w-full lg:pb-[100px] lg:pt-[60px] ">
+      {/* ==== Section One ==== */}
+      <div className="text-center mb-12">
+        <p className="text-xl font-extrabold lg:text-3xl w-[250px] md:w-[100%] mx-auto">
+          How <span className="text-[#014751]">GeomaticConnect</span> Works?
+        </p>
+        <p className="text-md lg:text-xl mt-4 w-[250px] mx-auto">
+          Trusted by leading brands and companies
+        </p>
+      </div>
+
+      {/* ==== Steps Section ==== */}
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 place-items-center gap-4 xl:gap-12">
+        <div className="bg-[#F2F6F6] border border-slate-300 p-6 rounded-lg max-w-[300px]">
+          <p className="text-2xl text-[#014751] font-bold border border-[#014751] p-4 rounded-full bg-white w-10 h-10 text-center flex justify-center items-center">1</p>
+          <p className="font-medium mt-10 mb-4 text-base">Create Your Free Account with us</p>
+          <p className="font-light">Pellentesque egestas elementum egestas faucibus sem.</p>
+        </div>
+        <div className="bg-[#F2F6F6] border border-slate-300 p-6 rounded-lg max-w-[300px]">
+          <p className="text-2xl text-[#014751] font-bold border border-[#014751] p-4 rounded-full bg-white w-10 h-10 text-center flex justify-center items-center">2</p>
+          <p className="font-medium mt-10 mb-4 text-base">Login with your Free Account</p>
+          <p className="font-light">Pellentesque egestas elementum egestas faucibus sem.</p>
+        </div>
+        <div className="bg-[#F2F6F6] border border-slate-300 p-6 rounded-lg max-w-[300px]">
+          <p className="text-2xl text-[#014751] font-bold border border-[#014751] p-4 rounded-full bg-white w-10 h-10 text-center flex justify-center items-center">3</p>
+          <p className="font-medium mt-10 mb-4 text-base">Make Request to your desire Company</p>
+          <p className="font-light">Pellentesque egestas elementum egestas faucibus sem.</p>
+        </div>
+        <div className="bg-[#F2F6F6] border border-slate-300 p-6 rounded-lg max-w-[300px]">
+          <p className="text-2xl text-[#014751] font-bold border border-[#014751] p-4 rounded-full bg-white w-10 h-10 text-center flex justify-center items-center">4</p>
+          <p className="font-medium mt-10 mb-4 text-base">Owned from properties transactions</p>
+          <p className="font-light">Pellentesque egestas elementum egestas faucibus sem.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
