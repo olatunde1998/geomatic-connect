0898d2f62e6cbfd1b490db284d3bc8fd7889706a
geodevcode <geodevinstitute@gmail.com>
2024-12-14 14:02:28 +0100

    component refactored

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 import { useEffect } from "react";
 import Navbar from "@/app/components/navbar/Navbar";
 import Hero from "@/app/components/landing-page-components/Hero";
-import HowItWorks from "@/app/components/landing-page-components/HowItWorks";
+// import HowItWorks from "@/app/components/landing-page-components/HowItWorks";
+import HowItWorksTwo from "@/app/components/landing-page-components/HowItWorksTwo";
 import ConnectServices from "@/app/components/landing-page-components/ConnectServices";
 import Testimonial from "@/app/components/landing-page-components/Testimonial";
 import Pricing from "@/app/components/landing-page-components/Pricing";
@@ -11,42 +12,44 @@ import ContactUs from "@/app/components/landing-page-components/ContactUs";
 import { Footer } from "@/app/components/landing-page-components/Footer";
 import { FaWhatsapp } from "react-icons/fa";
 
-export default function Home() {
-  useEffect(() => {
-    const triggerLoginEndpoint = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_BASEURL}/api/subscription`,
-          {
-            method: "GET",
-          }
-        );
-        if (!response.ok) {
-          console.error("Failed to wake up the server:", response.statusText);
-        } else {
-          console.log("Server woke up successfully!");
-        }
-      } catch (error) {
-        console.error("Error waking up the server:", error);
-      }
-    };
 
-    triggerLoginEndpoint();
-  }, []);
+export default function Home() {
+  // useEffect(() => {
+  //   const triggerLoginEndpoint = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `${process.env.NEXT_PUBLIC_BASEURL}/api/subscription`,
+  //         {
+  //           method: "GET",
+  //         }
+  //       );
+  //       if (!response.ok) {
+  //         console.error("Failed to wake up the server:", response.statusText);
+  //       } else {
+  //         console.log("Server woke up successfully!");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error waking up the server:", error);
+  //     }
+  //   };
+
+  //   triggerLoginEndpoint();
+  // }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-16 bg-[#F6F8FD]">
       <div className="w-full  flex-col items-center text-sm lg:flex">
         {/* === HERO SECTION === */}
-        <div className="bg-[#F2F6F6] w-full  flex-col items-center text-sm lg:flex">
+        <div className="bg-[#F2F6F6] lg:pt-8 w-full  flex-col items-center text-sm lg:flex">
           <div className=" w-full max-w-7xl text-sm  md:px-6">
             <Navbar />
             <Hero />
           </div>
         </div>
         {/* === HOW IT WORKS SECTION === */}
-        <div className="w-full  flex-col items-center text-sm lg:flex">
-          <div className=" w-full max-w-7xl text-sm  md:px-6">
-            <HowItWorks />
+        <div className="w-full flex-col items-center text-sm lg:flex">
+          <div className="w-full max-w-7xl text-sm  md:px-6">
+            {/* <HowItWorks /> */}
+            <HowItWorksTwo/>
           </div>
         </div>
 
@@ -55,14 +58,14 @@ export default function Home() {
           id="about-id"
           className="w-full  flex-col items-center text-sm lg:flex bg-[#638E96]"
         >
-          <div className=" w-full max-w-7xl text-sm  md:px-6">
+          <div className=" w-full max-w-7xl text-sm  md:px-6 pb-10 md:pb-0">
             <ConnectServices />
           </div>
         </div>
 
         {/* === Testimonial === */}
         <div className="w-full  flex-col items-center text-sm lg:flex">
-          <div className=" w-full max-w-7xl text-sm ">
+          <div className="w-full max-w-7xl text-sm ">
             <Testimonial />
           </div>
         </div>
@@ -109,7 +112,7 @@ export default function Home() {
         {/* === FOOTER SECTION === */}
         <div className="bg-[#014751] w-full text-sm lg:flex flex-col items-center relative">
           {/* === Background Image === */}
-          <div className="absolute inset-0 bg-center bg-no-repeat bg-cover bg-[url(/images/globe.png)] "></div>
+          <div className="absolute inset-0 bg-center bg-no-repeat bg-cover bg-[url(/images/globe-refine.png)] "></div>
           {/* ===Overlay with color and opacity=== */}
           <div className="absolute inset-0 bg-[#014751] opacity-80 rounded-tl-[32px] rounded-br-[32px]"></div>
           <div className="relative w-full max-w-7xl text-sm">
