5a212301b45ab921f8248ede88c896b0672d5d31
geodevcode <geodevinstitute@gmail.com>
2024-12-15 13:19:27 +0100

    dynamic page refactored (studentDetails and companyDetails)

--- app/(CompanyDashboard)/company-dashboard/[id]/page.tsx
+++ app/(CompanyDashboard)/company-dashboard/[id]/page.tsx
@@ -1,11 +1,21 @@
 import StudentDetails from "@/app/components/company-components/StudentDetails";
 import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
+
+// Helper function to validate the studentId
+const isValidStudentId = (id: any) => {
+  const regex = /^[a-f\d]{24}$/i; // Regex for a 24-character MongoDB ObjectId
+  return regex.test(id);
+};
 
 export default async function StudentDetailsPage({ params }: { params: any }) {
   const studentId = params.id;
   const session = await auth();
   if (!session?.user) redirect("/login");
+
+  if (!studentId || !isValidStudentId(studentId)) {
+    notFound(); // Returns a 404 page
+  }
   return (
     <>
       <main className="min-h-screen p-6 lg:p-12 xl:p-20 xl:px-2 xl:pl-16">
--- app/(StudentDashboard)/student-dashboard/[_id]/page.tsx
+++ app/(StudentDashboard)/student-dashboard/[_id]/page.tsx
@@ -1,12 +1,22 @@
 import StudentDetails from "@/app/components/student-components/StudentDetails";
 import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
+
+// Helper function to validate the companyId
+const isValidCompanyId = (id: any) => {
+  const regex = /^[a-f\d]{24}$/i; // Regex for a 24-character MongoDB ObjectId
+  return regex.test(id);
+};
 
 export default async function StudentDetailsPage({ params }: { params: any }) {
   const session = await auth();
   const companyId = params._id;
 
   if (!session?.user) redirect("/login");
+
+  if (!companyId || !isValidCompanyId(companyId)) {
+    notFound(); // Returns a 404 page
+  }
   return (
     <>
       <main className="min-h-screen p-6 lg:p-12 xl:p-20 xl:px-2 xl:pl-16">
