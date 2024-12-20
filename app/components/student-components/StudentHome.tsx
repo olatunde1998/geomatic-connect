fbdd0dfcce939ec84297adbebe0b47d2b719c64b
geodevcode <geodevinstitute@gmail.com>
2024-12-20 09:40:56 +0100

    subscription modal commented out

--- app/components/student-components/StudentHome.tsx
+++ app/components/student-components/StudentHome.tsx
@@ -29,21 +29,21 @@ export default function StudentHome({ session }: StudentHomeProps) {
   });
 
   // Trigger subscription modal
-  useEffect(() => {
-    const MAX_COUNT = 2;
-    const INTERVAL = 60000;
-    let count = 0;
+  // useEffect(() => {
+  //   const MAX_COUNT = 2;
+  //   const INTERVAL = 60000;
+  //   let count = 0;
 
-    const showModal = () => {
-      if (count < MAX_COUNT) {
-        setShowSubscribe(true);
-        count += 1;
-        setTimeout(showModal, INTERVAL);
-      }
-    };
-    const timeoutId = setTimeout(showModal, INTERVAL);
-    return () => clearTimeout(timeoutId);
-  }, []);
+  //   const showModal = () => {
+  //     if (count < MAX_COUNT) {
+  //       setShowSubscribe(true);
+  //       count += 1;
+  //       setTimeout(showModal, INTERVAL);
+  //     }
+  //   };
+  //   const timeoutId = setTimeout(showModal, INTERVAL);
+  //   return () => clearTimeout(timeoutId);
+  // }, []);
 
   return (
     <>
