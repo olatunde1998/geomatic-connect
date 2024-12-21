955faeb3fa20d2446b35ce81baf7f54edc22d886
geodevcode <geodevinstitute@gmail.com>
2024-12-21 16:33:49 +0100

    UsersDetailsPage created

---
+++ app/(AdminDashboard)/admin-dashboard/[_id]/page.tsx
@@ -0,0 +1,28 @@
+import UsersDetails from "@/app/components/admin-components/UsersDetails";
+import { auth } from "@/auth";
+import { notFound, redirect } from "next/navigation";
+
+// Helper function to validate the userId
+const isValidUserId = (id: any) => {
+  const regex = /^[a-f\d]{24}$/i; // Regex for a 24-character MongoDB ObjectId
+  return regex.test(id);
+};
+
+export default async function UsersDetailsPage({ params }: { params: any }) {
+  const session = await auth();
+  const token = session?.user?.token;
+  const userId = params?._id;
+
+  if (!session?.user) redirect("/login");
+
+  if (!userId || !isValidUserId(userId)) {
+    notFound(); // Returns a 404 page
+  }
+  return (
+    <>
+      <main className="min-h-screen pt-6  xl:py-10 xl:px-2 ">
+        <UsersDetails token={token} userId={userId} />
+      </main>
+    </>
+  );
+}
