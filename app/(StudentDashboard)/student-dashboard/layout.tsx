0db04a5eea1524085e9b412c3a99de7352bc2141
geodevcode <geodevinstitute@gmail.com>
2025-03-17 07:22:26 +0100

    bottomNavbar added

--- app/(StudentDashboard)/student-dashboard/layout.tsx
+++ app/(StudentDashboard)/student-dashboard/layout.tsx
@@ -1,9 +1,10 @@
-import StudentNavBar from "@/app/components/navbar/StudentNavBar";
 import { StudentSidebar } from "@/app/components/sidebar/StudentSidebar";
-import { auth } from "@/auth";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import StudentNavBar from "@/app/components/navbar/StudentNavBar";
+import BottomNavBar from "@/app/components/navbar/BottomNavBar";
 import { Analytics } from "@vercel/analytics/next";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import { auth } from "@/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -49,10 +50,11 @@ export default async function StudentLayout({
           <div className="hidden w-[200px] md:w-[160px] lg:w-[160px] xl:w-[200px] fixed flex-col lg:block border-r ml-6 pr-2 lg:ml-10 pt-32 min-h-screen">
             <StudentSidebar session={session} />
           </div>
-          <main className="lg:pl-48 xl:pl-52 xl:pr-6 overflow-x-auto">
+          <main className="lg:pl-48 xl:pl-52 xl:pr-6 overflow-x-auto pb-28">
             {children}
             <Analytics />
           </main>
+          <BottomNavBar />
         </div>
       </div>
     </div>
