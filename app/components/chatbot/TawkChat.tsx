5c38d13ecb34b461dbd27e934fda532081909521
geodevcode <geodevinstitute@gmail.com>
2025-03-19 09:07:51 +0100

    Tawk secrete key variable added

--- app/components/chatbot/TawkChat.tsx
+++ app/components/chatbot/TawkChat.tsx
@@ -57,7 +57,7 @@ export default function TawkChat() {
             var s1 = document.createElement("script"), 
                 s0 = document.getElementsByTagName("script")[0];
             s1.async = true;
-            s1.src = 'https://embed.tawk.to/67d9aaa81315a2190d0f36f0/1iml458vj';
+            s1.src = '${process.env.NEXT_PUBLIC_TAWK_TO_SCRIPT_URL}';
             s1.charset = 'UTF-8';
             s1.setAttribute('crossorigin', '*');
             s0.parentNode.insertBefore(s1, s0);
