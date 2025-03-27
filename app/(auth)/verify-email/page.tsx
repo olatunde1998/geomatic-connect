70343d0669e6e1c588aa5b795592c3a3f291c379
geodevcode <geodevinstitute@gmail.com>
2025-03-27 14:42:38 +0100

    verifyemail restriction logic added

--- app/(auth)/verify-email/page.tsx
+++ app/(auth)/verify-email/page.tsx
@@ -1,11 +1,17 @@
 import VerifyEmail from "@/app/components/auth-components/VerifyEmail";
 import LeftContainer from "./leftContainer";
+import { redirect } from "next/navigation";
 
-export default async function VerifyEmailPage() {
+export default async function VerifyEmailPage({ searchParams,}: {searchParams: any}) {
+  const userEmail = searchParams.email;
+  const verifyToken = searchParams?.verifyToken;
+  if (!verifyToken || !userEmail) {
+    redirect("/login");
+  }
   return (
     <div className="text-[#1F4D36] md:flex justify-between h-screen overflow-y-hidden">
       <LeftContainer />
-      <VerifyEmail />
+      <VerifyEmail userEmail={userEmail} />
     </div>
   );
 }
