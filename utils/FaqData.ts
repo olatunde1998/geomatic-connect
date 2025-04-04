1109e921a0f14333afabbb33395b2cbf5478af5b
geodevcode <geodevinstitute@gmail.com>
2025-04-04 12:00:51 +0100

    content updated

--- utils/FaqData.ts
+++ utils/FaqData.ts
@@ -2,29 +2,31 @@
 export const faqData = [
   {
     id: 1,
-    question:
-      "Do you offer a free trial?",
+    question: "How do I register?",
+    answer:
+      "Yes. To register, simply click on the 'Create free account' button on our website. You will be prompted to enter your email address and create a password. Once you have completed the registration form, you will receive a confirmation email to verify your account. After verifying your email, you can log in and start using our services.",
+  },
+  {
+    id: 2,
+    question: "Do you offer a free trial?",
     answer:
       "Yes. In fact, we go one step further. You may use our freemium plan without payment for personal or commercial purposes but only on the condition that you include a link to Icons& in your work. Please note that freemium plan do come with limitations such as smaller sizes, select file types, and variety.",
   },
   {
-    id: 2,
-    question:
-      "How does Geomatics Connect work?",
+    id: 3,
+    question: "How does Geomatics Connect work?",
     answer:
       "Geomatics Connect offers personalized one-on-one coaching, structured job application guidance, detailed feedback mechanisms, and access to a supportive community. Mentees receive tailored advice and resources to enhance their job search strategy and improve their chances of securing a job offer",
   },
   {
-    id: 3,
-    question:
-      "Which plan do I need?",
+    id: 4,
+    question: "Which plan do I need?",
     answer:
       "Purchasing each asset's specific plan (Basic, Growth or Scale) unlocks all options and removes the attribution requirement. Alternatively, you can purchase the Full Set plan to receive all assets and use them without crediting us. Please make sure you've read our Licensing Agreement before purchasing.",
   },
   {
-    id: 4,
-    question:
-      "Do you offer student or company discounts?" ,
+    id: 5,
+    question: "Do you offer student or company discounts?",
     answer:
       "Yes. We are happy to help companies and students. Chat with us to get started.",
   },
