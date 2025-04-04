9dd5e88d0331987bf2d8b3ffab4edfa0d9d77931
geodevcode <geodevinstitute@gmail.com>
2025-04-04 17:12:37 +0100

    blogData file created

---
+++ utils/BlogData.ts
@@ -0,0 +1,46 @@
+import GetStarted from "@/public/images/get-started.webp";
+import DreamJob from "@/public/images/dream-job.webp";
+import PayingJob from "@/public/images/paying-job.webp";
+import DemandSkills from "@/public/images/demand-skills.webp";
+
+
+//Blog section data
+export const blogData = [
+  {
+    id: 1,
+    imageUrl: PayingJob,
+    headings: "How to Land a High-Paying Remote Job",
+    content:
+      "Learn the top strategies for landing a high-paying remote job that you actually love.",
+    createdAt: "February 18, 2025",
+    readTime: "7 min read",
+  },
+  {
+    id: 2,
+    imageUrl: DreamJob,
+    headings:
+      "How to Land Your Dream Job: A Step-by-Step Guide for Career Changers",
+    content:
+      "Proven steps to land your dream job. Learn how to optimize your resume, ace interviews, and grow your network for a successful transition into a new field.",
+    createdAt: "February 18, 2025",
+    readTime: "7 min read",
+  },
+  {
+    id: 3,
+    imageUrl: DemandSkills,
+    headings: "10 High-Demand Job Skills for 2025: What You Need to Stay Ahead",
+    content:
+      "Discover the most sought-after skills that will shape the job market in 2025 and learn how to prepare for the future of work",
+    createdAt: "February 18, 2025",
+    readTime: "7 min read",
+  },
+  {
+    id: 4,
+    imageUrl: GetStarted,
+    headings: "Getting Started with Geomatic Connect: A Beginner's Guide",
+    content:
+      "Learn how to make the most of NextJob's features for your job search",
+    createdAt: "February 18, 2025",
+    readTime: "7 min read",
+  },
+];
