ed92b683bec624a577f3b98c3f3f6a748c30adac
geodevcode <geodevinstitute@gmail.com>
2025-04-05 07:29:49 +0100

    Modal added to chatbot

--- app/components/chatbot/ChatBot.tsx
+++ app/components/chatbot/ChatBot.tsx
@@ -22,6 +22,7 @@ import {
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Input } from "@/components/ui/input";
 import { Components } from "react-markdown";
+import { Modal } from "@/app/components/modals/Modal";
 
 interface CodeProps {
   node?: any;
@@ -111,110 +112,112 @@ export default function Chatbot() {
       </AnimatePresence>
       <AnimatePresence>
         {isChatOpen && (
-          <motion.div
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.8 }}
-            transition={{ duration: 0.2 }}
-            className="fixed bottom-32 right-2 md:right-4 z-50 w-[95%] md:w-[500px]"
-          >
-            <Card className="border-2">
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-3">
-                <CardTitle className="text-lg font-bold">Ask AI</CardTitle>
-                <Button
-                  onClick={toggleChat}
-                  size="sm"
-                  variant="ghost"
-                  className="px-2 py-0"
-                >
-                  <X className="size-4" />
-                  <span className="sr-only">Close Chat</span>
-                </Button>
-              </CardHeader>
+          <Modal show={isChatOpen} onClose={() => toggleChat()}>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              transition={{ duration: 0.2 }}
+              className="fixed bottom-32 right-2 md:right-4 z-50 w-[95%] md:w-[500px]"
+            >
+              <Card className="border-2">
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-3">
+                  <CardTitle className="text-lg font-bold">Ask AI</CardTitle>
+                  <Button
+                    onClick={toggleChat}
+                    size="sm"
+                    variant="ghost"
+                    className="px-2 py-0"
+                  >
+                    <X className="size-4" />
+                    <span className="sr-only">Close Chat</span>
+                  </Button>
+                </CardHeader>
 
-              <CardContent>
-                <ScrollArea className="h-[300px] pr-4">
-                  {messages.length === 0 && (
-                    <p className="w-full mt-32 text-gray-500 items-center justify-center flex gap-3">
-                      No message Yet.
-                    </p>
-                  )}
-                  {messages?.map((message, index) => (
-                    <div
-                      key={index}
-                      className={`mb-4 ${
-                        message.role === "user" ? "text-right" : "text-left"
-                      }`}
-                    >
+                <CardContent>
+                  <ScrollArea className="h-[300px] pr-4">
+                    {messages.length === 0 && (
+                      <p className="w-full mt-32 text-gray-500 items-center justify-center flex gap-3">
+                        No message Yet.
+                      </p>
+                    )}
+                    {messages?.map((message, index) => (
                       <div
-                        className={`inline-block p-4 rounded-lg ${
-                          message.role === "user"
-                            ? "bg-[#014751] text-primary-foreground"
-                            : "bg-muted"
+                        key={index}
+                        className={`mb-4 ${
+                          message.role === "user" ? "text-right" : "text-left"
                         }`}
                       >
-                        <ReactMarkdown
-                          remarkPlugins={[remarkGfm]}
-                          components={MarkdownComponents}
+                        <div
+                          className={`inline-block p-4 rounded-lg ${
+                            message.role === "user"
+                              ? "bg-[#014751] text-primary-foreground"
+                              : "bg-muted"
+                          }`}
                         >
-                          {message.content}
-                        </ReactMarkdown>
+                          <ReactMarkdown
+                            remarkPlugins={[remarkGfm]}
+                            components={MarkdownComponents}
+                          >
+                            {message.content}
+                          </ReactMarkdown>
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                  {isLoading && (
-                    <div className="w-full items-center flex justify-center gap-3">
-                      <Loader2 className="animate-spin h-5 w-5 text-green-400" />
-                      <button
-                        type="button"
-                        className="underline"
-                        onClick={() => stop()}
-                      >
-                        abort
-                      </button>
-                    </div>
-                  )}
-                  {error && (
-                    <div className="w-full items-center flex justify-center gap-3">
-                      <p>An error occurred.</p>
-                      <button
-                        type="button"
-                        className="underline"
-                        onClick={() => reload()}
-                      >
-                        Retry
-                      </button>
-                    </div>
-                  )}
-                  <div ref={scrollRef}> </div>
-                </ScrollArea>
-              </CardContent>
+                    ))}
+                    {isLoading && (
+                      <div className="w-full items-center flex justify-center gap-3">
+                        <Loader2 className="animate-spin h-5 w-5 text-green-400" />
+                        <button
+                          type="button"
+                          className="underline"
+                          onClick={() => stop()}
+                        >
+                          abort
+                        </button>
+                      </div>
+                    )}
+                    {error && (
+                      <div className="w-full items-center flex justify-center gap-3">
+                        <p>An error occurred.</p>
+                        <button
+                          type="button"
+                          className="underline"
+                          onClick={() => reload()}
+                        >
+                          Retry
+                        </button>
+                      </div>
+                    )}
+                    <div ref={scrollRef}> </div>
+                  </ScrollArea>
+                </CardContent>
 
-              <CardFooter>
-                <form
-                  onSubmit={handleSubmit}
-                  className="flex items-center w-full space-x-2"
-                >
-                  <Input
-                    type="text"
-                    placeholder="Type your message here..."
-                    onChange={handleInputChange}
-                    disabled={isLoading}
-                    value={input}
-                    className="flex-1"
-                  />
-                  <Button
-                    type="submit"
-                    disabled={isLoading}
-                    size="icon"
-                    className="size-9 bg-slate-700 rounded-full flex items-center justify-center"
+                <CardFooter>
+                  <form
+                    onSubmit={handleSubmit}
+                    className="flex items-center w-full space-x-2"
                   >
-                    <Send className="size-4" />
-                  </Button>
-                </form>
-              </CardFooter>
-            </Card>
-          </motion.div>
+                    <Input
+                      type="text"
+                      placeholder="Type your message here..."
+                      onChange={handleInputChange}
+                      disabled={isLoading}
+                      value={input}
+                      className="flex-1"
+                    />
+                    <Button
+                      type="submit"
+                      disabled={isLoading}
+                      size="icon"
+                      className="size-9 bg-slate-700 rounded-full flex items-center justify-center"
+                    >
+                      <Send className="size-4" />
+                    </Button>
+                  </form>
+                </CardFooter>
+              </Card>
+            </motion.div>
+          </Modal>
         )}
       </AnimatePresence>
     </div>
