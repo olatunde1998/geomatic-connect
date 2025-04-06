d6ad215573ea9a6145db77bc83eb1469bfa2b2be
geodevcode <geodevinstitute@gmail.com>
2025-04-06 12:17:47 +0100

    BottomNav refactored

--- app/components/navbar/BottomNav.tsx
+++ app/components/navbar/BottomNav.tsx
@@ -1,29 +1,25 @@
 "use client";
 import { bottomLinks } from "@/utils/sidebarLinks";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
 export default function BottomNav() {
   const pathname = usePathname();
-  const basePath = pathname.split("/")[1] || "";
+  const router = useRouter();
 
-  const handleSmoothScroll = (sectionId: string) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-  };
   return (
     <ul className="fixed bottom-0 left-0 w-full flex items-center justify-between bg-white/50 backdrop-blur-md border-t-[0.5px] border-slate-100 px-7 py-3 md:hidden">
       {bottomLinks.map((item, index) => {
         const Icon = item.icon;
-        const linkHref = basePath ? `/${basePath}${item.href}` : item.href;
         return (
           <li
-            onClick={() => handleSmoothScroll(`${item.href}`)}
+            onClick={() => router.push(item.href)}
             key={index}
             className={`${
-              pathname === linkHref ? "text-[#014751]" : "#828282"
+              pathname === item.href ||
+              (item.href !== "/" && pathname.startsWith(item.href))
+                ? "text-[#014751]"
+                : "text-[#515151]"
             } flex flex-col items-center cursor-pointer`}
           >
             <Icon size={24} />
