cbc611300024868dfd5fe03d6df25780641c041a
geodevcode <geodevinstitute@gmail.com>
2025-04-06 11:18:20 +0100

    bugs resolved

--- app/(AdminDashboard)/admin-dashboard/blog/[slug]/page.tsx
+++ app/(AdminDashboard)/admin-dashboard/blog/[slug]/page.tsx
@@ -4,15 +4,15 @@ import { redirect } from "next/navigation";
 
 export default async function StudentDetailsPage({ params }: { params: any }) {
   const session = await auth();
-  const blogId = params?._id;
+  const blogSlug = params?.slug;
 
-  if (!session?.user) {
+  if (!blogSlug) {
     redirect("/login");
   }
   return (
     <>
       <main className="min-h-screen p-6 lg:p-12 xl:p-20 xl:px-2">
-        <BlogDetails blogId={blogId} />
+        <BlogDetails blogSlug={blogSlug} />
       </main>
     </>
   );
