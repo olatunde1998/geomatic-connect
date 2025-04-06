90d6ae77f16845b0e4179c1ff3404501eb210f68
geodevcode <geodevinstitute@gmail.com>
2025-04-06 11:04:43 +0100

    Blog Layout created

---
+++ app/blog/layout.tsx
@@ -0,0 +1,62 @@
+import BottomNavBar from "@/app/components/navbar/BottomNavBar";
+import { Analytics } from "@vercel/analytics/next";
+import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from "next";
+import Navbar from "../components/navbar/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  manifest: "/manifest.json",
+  title: {
+    template: "%s",
+    default: "Geomatic Connect",
+  },
+  description:
+    "Geomatic Connect  is a platform that enables higher institution students to connect with companies offering internships, SIWES, SWEP placements, and other practical training opportunities. It also helps companies find and recruit qualified candidates with ease.",
+  viewport: {
+    width: "device-width",
+    height: "device-height",
+    initialScale: 1,
+  },
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_APP_URL}`),
+  twitter: {
+    card: "summary_large_image",
+    site: `${process.env.NEXT_PUBLIC_APP_URL}`,
+    creator: "Geomatic Connect Teams",
+    title: "Geomatic Connect",
+    description:
+      "Geomatic Connect  is a platform that enables higher institution students to connect with companies offering internships, SIWES, SWEP placements, and other practical training opportunities. It also helps companies find and recruit qualified candidates with ease.",
+    images: ["/images/opengraph-image.png"],
+  },
+  openGraph: {
+    images: ["/images/opengraph-image.png"],
+  },
+};
+
+export default async function BlogLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+        {children}
+        <Analytics />
+        <BottomNavBar />
+      </div>
+    </>
+  );
+}
