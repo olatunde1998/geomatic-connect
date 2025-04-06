01c15eb85ff7903b4355283e4f6ab266139aecbc
geodevcode <geodevinstitute@gmail.com>
2025-04-06 11:08:53 +0100

    Blog HomePage refactored

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Navbar from "@/app/components/navbar/Navbar";
 import BottomNav from "@/app/components/navbar/BottomNav";
 import BlogHome from "@/app/components/landing-page-components/BlogHome";
 
@@ -8,19 +7,12 @@ export const metadata: Metadata = {
   description:
     "Geomatic Connect  is a platform that enables higher institution students to connect with companies offering internships, SIWES, SWEP placements, and other practical training opportunities. It also helps companies find and recruit qualified candidates with ease.",
 };
-export default async function HomePage() {
+export default async function BlogHomePage() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between pt-16 bg-[#F6F8FD]">
-        <div className="w-full flex-col items-center text-sm lg:flex">
-          <div className="bg-[#F2F6F6 bg-primary-foreground lg:pt-8 w-full text-sm">
-            <div className="max-w-[1300px] mx-auto text-sm md:px-6">
-              <Navbar />
-            </div>
-          </div>
-          <div className="w-full max-w-5xl">
-            <BlogHome />
-          </div>
+        <div className="w-full max-w-5xl text-sm">
+          <BlogHome />
         </div>
       </main>
       <BottomNav />
