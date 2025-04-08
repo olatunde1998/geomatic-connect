40eb86c8b7cfa64b3a313fe0c433b6c9e4f74f54
geodevcode <geodevinstitute@gmail.com>
2025-04-08 10:13:56 +0100

    Refactor code structure for improved readability and maintainability

--- auth.ts
+++ auth.ts
@@ -1,7 +1,6 @@
 import credentials from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 // import Google from "next-auth/providers/google";
-
 // import GithubProvider from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
