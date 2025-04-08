a5850885702e14eb908b3961d716dcc4b859547b
geodevcode <geodevinstitute@gmail.com>
2025-04-08 08:50:18 +0100

    fix: remove unnecessary whitespace in authentication module

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import credentials from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 // import Google from "next-auth/providers/google";
+
 // import GithubProvider from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
