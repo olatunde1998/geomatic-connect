d41243893ae892a355259d3b3fce223279cb4139
geodevcode <geodevinstitute@gmail.com>
2025-04-12 09:06:58 +0100

    feat: add companion data structure for displaying partner logos and titles

---
+++ utils/CompanionData.ts
@@ -0,0 +1,27 @@
+import GeoinfotechLogo from "@/public/images/geoinfotech.gif";
+import GeosysLogo from "@/public/images/geosys-logo.png";
+import FssLogo from "@/public/images/fss-logo.png";
+import NISLogo from "@/public/images/nis-logo.png";
+
+export const companionData = [
+  {
+    id: 1,
+    title: "FSS",
+    imageUrl: FssLogo,
+  },
+  {
+    id: 2,
+    title: "NIS",
+    imageUrl: NISLogo,
+  },
+  {
+    id: 3,
+    title: "Geosys",
+    imageUrl: GeosysLogo,
+  },
+  {
+    id: 4,
+    title: "Geoinfotech",
+    imageUrl: GeoinfotechLogo,
+  },
+];
