cca269f107eb8e123df97b69d5535a829d62c4d2
geodevcode <geodevinstitute@gmail.com>
2025-04-12 08:01:18 +0100

    feat: add WhoWeAre data and analytics for improved content display

---
+++ utils/WhoWeAreData.ts
@@ -0,0 +1,54 @@
+import { GiArcheryTarget } from "react-icons/gi";
+import { TbSitemap } from "react-icons/tb";
+import { BsBarChart } from "react-icons/bs";
+import { GoPeople } from "react-icons/go";
+
+//WhoWeAre data
+export const whoWeAreData = [
+  {
+    id: 1,
+    iconUrl: GiArcheryTarget,
+    title: "Vision",
+    content:
+      "Connecting geomatic students with top companies for training and professional growth.",
+  },
+  {
+    id: 2,
+    iconUrl: TbSitemap,
+    title: "Mission",
+    content:
+      "We aim to deliver automation platforms that boost placement opportunities for geomatic students.",
+  },
+  {
+    id: 3,
+    iconUrl: GoPeople,
+    title: "Our Core Values",
+    content: " Students Focus, Innovation, Integrity & Respect.",
+  },
+  {
+    id: 4,
+    iconUrl: BsBarChart,
+    title: "Our Culture",
+    content:
+      "At Geomatic Connect, we create solutions to boost geomatic student placements and contribute to national growth.",
+  },
+];
+
+//WhoWeAre Analytics data
+export const whoWeAreAnalyticsData = [
+  {
+    id: 1,
+    totalCount: "10+",
+    title: "Project Completed",
+  },
+  {
+    id: 2,
+    totalCount: "50+",
+    title: "Company work with",
+  },
+  {
+    id: 3,
+    totalCount: "60+",
+    title: "Students connected",
+  },
+];
