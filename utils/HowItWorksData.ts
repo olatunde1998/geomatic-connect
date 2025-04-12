e23de8b9c3ccc21d3f37830eca792033322deb20
geodevcode <geodevinstitute@gmail.com>
2025-04-12 08:32:46 +0100

    feat: add HowItWorks data structure for user guidance and navigation

---
+++ utils/HowItWorksData.ts
@@ -0,0 +1,38 @@
+import { Check } from "lucide-react";
+
+//HowItWorks data
+export const howItWorksData = {
+  howItWork: {
+    name: "How it works?",
+    content:
+      "Watch the video to learn how to submit requests to your desired companies using Geomatic Connect. Discover how to navigate and make the most of the application&apos;s features.",
+    data: [
+      {
+        id: 1,
+        iconUrl: Check,
+        title: "Create Account",
+        content: "Create Your Free Account with us",
+      },
+      {
+        id: 2,
+        iconUrl: Check,
+        title: "Verify Account",
+        content: "Verify Your Email Address",
+      },
+      {
+        id: 3,
+        iconUrl: Check,
+        title: "Login",
+        content: "Login with your Free Account",
+      },
+      {
+        id: 4,
+        iconUrl: Check,
+        title: "Make Request",
+        content: "Make Request to your desire Company",
+      },
+    ],
+    buttonOne: "Contact Us",
+    buttonTwo: "Create free account",
+  },
+};
