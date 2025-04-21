064006b23032cb9bdac0ed25b51d189c0656b22b
geodevcode <geodevinstitute@gmail.com>
2025-04-21 07:35:27 +0100

    feat: add LineTrendChart component for visualizing application trends

---
+++ app/components/charts/LineTrendChart.tsx
@@ -0,0 +1,48 @@
+"use client";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function LineTrendChart({ data }: { data: any[] }) {
+  return (
+    <div className="my-6" style={{ width: "100%", height: 300, padding: "10px", borderWidth: "1px", borderColor: "#E5E7EB", borderStyle: "solid", borderRadius: "8px" }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={data}
+          margin={{
+            top: 20,
+            right: 20,
+            left: 2,
+            bottom: 10,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <XAxis dataKey="month" axisLine={false} tickLine={false} dy={10} />
+          <YAxis
+            axisLine={false}
+            tickLine={false}
+            tickCount={7}
+            domain={[0, 20]}
+            dx={-10}
+          />
+          <Tooltip />
+          <Line
+            type="monotone"
+            dataKey="totalApplication"
+            stroke="#4285F4"
+            strokeWidth={2}
+            dot={{ r: 4, strokeWidth: 2 }}
+            activeDot={{ r: 6 }}
+            name="Total Application"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
