f236e0112ce99767d8286377c8d2a365c91b74db
geodevcode <geodevinstitute@gmail.com>
2025-04-21 07:34:44 +0100

    feat: add chartData utility for monthly application statistics

---
+++ utils/chartData.ts
@@ -0,0 +1,15 @@
+//Chart section data
+export const chartData = [
+  { id: 1, month: "Jan", totalApplication: 4 },
+  { id: 2, month: "Feb", totalApplication: 5 },
+  { id: 3, month: "Mar", totalApplication: 8 },
+  { id: 4, month: "Apr", totalApplication: 12 },
+  { id: 5, month: "May", totalApplication: 10 },
+  { id: 6, month: "Jun", totalApplication: 14 },
+  { id: 7, month: "Jul", totalApplication: 9 },
+  { id: 8, month: "Aug", totalApplication: 11 },
+  { id: 9, month: "Sep", totalApplication: 6 },
+  { id: 10, month: "Oct", totalApplication: 7 },
+  { id: 11, month: "Nov", totalApplication: 4 },
+  { id: 12, month: "Dec", totalApplication: 8 },
+];
