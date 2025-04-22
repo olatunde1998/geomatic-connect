490c86e196d730e96b4e1869942f5445123f2fe8
geodevcode <geodevinstitute@gmail.com>
2025-04-22 10:20:58 +0100

    fix: update HomePage to include userId in OverviewHome component

--- app/(StudentDashboard)/student-dashboard/overview/page.tsx
+++ app/(StudentDashboard)/student-dashboard/overview/page.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import { auth } from "@/auth";
 import OverviewHome from "@/app/components/student-components/OverviewHome";
 import { redirect } from "next/navigation";
+import type { Metadata } from "next";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "Overview | Geomatic Connect",
@@ -11,12 +11,13 @@ export const metadata: Metadata = {
 export default async function HomePage() {
   const session = await auth();
   const token = session?.user?.token;
-  if (!session?.user) {
+  const userId = session?.user?._id;
+  if (!session?.user || !token || !userId) {
     redirect("/login");
   }
   return (
     <main className="min-h-screen p-6 pt-24 lg:p-12 xl:p-20 lg:pt-32 xl:pt-32 font-sans text-md">
-      <OverviewHome token={token} />
+      <OverviewHome token={token} userId={userId} />
     </main>
   );
 }
