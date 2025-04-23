31570fd83291065dc14b6dacf89156fbd3a583e8
geodevcode <geodevinstitute@gmail.com>
2025-04-23 08:01:00 +0100

    fix: update redirect URL to point to the correct route

--- auth.ts
+++ auth.ts
@@ -165,7 +165,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     async redirect({ url, baseUrl }) {
       if (url.startsWith("/")) return `${baseUrl}${url}`;
       else if (new URL(url).origin === baseUrl) return url;
-      return `${baseUrl}/dashboard-redirect`;
+      return `${baseUrl}/redirect`;
     },
   },
 
