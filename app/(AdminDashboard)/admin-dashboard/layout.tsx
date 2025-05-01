134680476074061979c3ff20a3747e28d635fa7c
geodevcode <geodevinstitute@gmail.com>
2025-05-01 08:35:41 +0100

    fix: adjust layout for AdminSidebar and main content in AdminLayout for improved responsiveness

--- app/(AdminDashboard)/admin-dashboard/layout.tsx
+++ app/(AdminDashboard)/admin-dashboard/layout.tsx
@@ -59,10 +59,10 @@ export default async function AdminLayout({
         <AdminNavBar session={session} />
         <div className="flex flex-col space-y-6">
           <div className="grid flex-1 gap-12 ">
-            <div className="hidden w-[200px] md:w-[160px] lg:w-[160px] xl:w-[200px] fixed flex-col md:block border-r ml-6 pr-2 lg:ml-10 pt-32 min-h-screen">
+            <div className="hidden w-[200px] md:w-[160px] lg:w-[160px] xl:w-[200px] fixed flex-col lg:block border-r ml-6 pr-2 lg:ml-10 pt-32 min-h-screen">
               <AdminSidebar />
             </div>
-            <main className="px-6 md:pl-52 lg:pl-60 xl:pl-72 md:pr-12 overflow-x-auto pb-28 md:pb-10">
+            <main className="px-6 lg:pl-60 xl:pl-72 md:pr-12 overflow-x-auto pb-28 md:pb-10">
               {children}
               {/* <Analytics /> */}
             </main>
