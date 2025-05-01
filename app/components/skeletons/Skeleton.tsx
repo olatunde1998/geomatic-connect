6ccea56a126c05fd2e7d9a6621bce5246f1d6eb8
geodevcode <geodevinstitute@gmail.com>
2025-05-01 08:01:17 +0100

    fix: remove unnecessary z-index from Skeleton component for improved layout

--- app/components/skeletons/Skeleton.tsx
+++ app/components/skeletons/Skeleton.tsx
@@ -1,6 +1,6 @@
 export const Skeleton = () => {
   return (
-    <div className="relative -z-[9999] w-full rounded overflow-hidden">
+    <div className="w-full rounded overflow-hidden">
       <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
       <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
       <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
