f20ca15b634c2f93d65e9e779f8cdffdb43c164a
geodevcode <geodevinstitute@gmail.com>
2025-05-04 13:14:15 +0100

    fix: remove Tawk_API onLoad function to streamline chat initialization

--- app/components/chatbot/TawkChat.tsx
+++ app/components/chatbot/TawkChat.tsx
@@ -16,11 +16,6 @@ export default function TawkChat() {
       <Script id="tawk-to-script" strategy="lazyOnload">
         {`
           var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-          Tawk_API.onLoad = function() {
-              setTimeout(() => {
-                Tawk_API?.maximize?.();
-              }, 5000);
-          };
           (function() {
             var s1 = document.createElement("script"), 
                 s0 = document.getElementsByTagName("script")[0];
