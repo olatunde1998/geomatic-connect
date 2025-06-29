75fa413576bb091939070ada465823c6cf1aa07a
geodevcode <geodevinstitute@gmail.com>
2025-06-29 19:04:50 +0100

    feat: pass jobId to JobDetails component in job detail pages

--- app/(StudentDashboard)/student-dashboard/jobs/[id]/page.tsx
+++ app/(StudentDashboard)/student-dashboard/jobs/[id]/page.tsx
@@ -1,9 +1,10 @@
 import JobDetails from "@/app/components/job-listings/JobDetails";
 
-export default function JobDetailsPage() {
+export default function JobDetailsPage({ params }: { params: any }) {
+  const jobId = params?.id;
   return (
     <div className="min-h-screen p-6 pt-24 lg:p-12 xl:p-20 lg:pt-32 xl:pt-32 font-sans text-md">
-      <JobDetails />
+      <JobDetails jobId={jobId} />
     </div>
   );
 }
