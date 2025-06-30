f3711dfb8a557fcdf7cf0050427871c8f03d9804
geodevcode <geodevinstitute@gmail.com>
2025-06-30 15:39:28 +0100

    refactor: remove role and userId logic from JobHome component for cleaner code

--- app/components/job-listings/JobHome.tsx
+++ app/components/job-listings/JobHome.tsx
@@ -5,15 +5,12 @@ import { GetjobsRequest } from "@/app/services/job.request";
 import { Modal } from "@/app/components/modals/Modal";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
-import { Plus } from "lucide-react";
 import { useState } from "react";
 
 export default function JobHome() {
   const [showCreateJob, setShowCreateJob] = useState(false);
   const { data: session } = useSession();
   const token = session?.user?.token as string;
-  const role = session?.user?.role as string;
-  const userId = session?.user?._id;
   const [currentPage, setCurrentPage] = useState(1);
   const [limit] = useState(6);
 
@@ -24,25 +21,6 @@ export default function JobHome() {
 
   return (
     <>
-      {role === "Company" && (
-        <div className="md:flex items-center md:space-x-4 mb-6">
-          <div className="w-full">
-            <p className="text-gray-600 text-lg font-semibold dark:text-accent-foreground">
-              Job Listings
-            </p>
-            <p className="text-sm text-gray-500 font-normal">
-              View and manage all job listings posted by your company.
-            </p>
-          </div>
-          <div
-            onClick={() => setShowCreateJob(true)}
-            className="my-4 flex p-2 md:p-3 justify-center items-center gap-[8px] rounded-[8px] text-white w-full md:w-[200px] lg:w-[200px] cursor-pointer  px-3.5 py-4 font-light shadow-sm bg-gradient-to-r from-[#49AD51] to-[#B1D045]"
-          >
-            <p className="text-[#FFFFFF] text-sm md:text-md">Create Job</p>
-            <Plus className="w-4 h-4 md:w-5 md:h-5" />
-          </div>
-        </div>
-      )}
       {jobsData &&
         jobsData.data?.map((item: any, index: number) => (
           <div key={index}>
@@ -59,11 +37,7 @@ export default function JobHome() {
           </div>
         ))}
       <Modal show={showCreateJob} onClose={() => setShowCreateJob(false)}>
-        <CreateJob
-          userId={userId}
-          token={token}
-          setShowCreateJob={setShowCreateJob}
-        />
+        <CreateJob token={token} setShowCreateJob={setShowCreateJob} />
       </Modal>
     </>
   );
