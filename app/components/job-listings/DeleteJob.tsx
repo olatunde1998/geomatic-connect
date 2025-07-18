b5ad6dcff2b9cc277ff256f87e8323d1d867052f
geodevcode <geodevinstitute@gmail.com>
2025-07-18 12:55:19 +0100

    dark mode features added to delete job  pages

--- app/components/job-listings/DeleteJob.tsx
+++ app/components/job-listings/DeleteJob.tsx
@@ -14,7 +14,7 @@ export default function DeleteJob({
   const [isDeleting, setIsDeleting] = useState(false);
 
   return (
-    <div className="bg-white p-8 rounded-[0.63rem]  mx-auto">
+    <div className="bg-white dark:bg-background p-8 rounded-[0.63rem]  mx-auto">
       <div className="bg-[#FEF3F2] p-3 w-fit rounded-full mx-auto">
         <div className="bg-[#FEE4E2] p-3 rounded-full">
           <CircleAlert color="#DE3024" size={32} />
@@ -27,7 +27,7 @@ export default function DeleteJob({
       {/*======= Cancel Button and Delete Button ====== */}
       <div className="mt-12 flex space-x-4">
         <div
-          className="border-[1.5px] border-slate-300 dark:text-primary-foreground rounded-[8px] px-[28px] py-[12px] cursor-pointer text-center w-full lg:w-[230px]"
+          className="border-[1.5px] border-slate-300 dark:border-muted dark:text-muted-foreground rounded-[8px] px-[28px] py-[12px] cursor-pointer text-center w-full lg:w-[230px]"
           onClick={() => setShowConfirmDelete(false)}
         >
           Cancel
